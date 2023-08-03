c17cd292aeb7628761adc323b7ffe70211a4790e
Eduarda Tomasio <dudasoarestomasio@gmail.com>
2023-08-03 11:38:35 -0300

    stubs a network response from a sessions fixture

--- cypress/integration/sessions.spec.js
+++ cypress/integration/sessions.spec.js
@@ -12,7 +12,7 @@ describe("Sessions page", () => {
         cy.get("[data-cy=Thursday]").as("ThursdayBtn");;
         cy.get("[data-cy=Friday]").as("FridayBtn");;
 
-    })
+    });
 
     it("Should navigate to conference sessions page and view day filter buttons", () => {
         // Validate that filter buttons exists
@@ -21,18 +21,7 @@ describe("Sessions page", () => {
         cy.get("@ThursdayBtn");
         cy.get("@FridayBtn");
     });
-
-    it("should filter sessions and only display wednesday sessions when Wednesday button is clicked", () => {
-        cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionsInfo");
-        cy.get("@WednesdayBtn").click();
-        cy.wait("@getSessionsInfo")
-
-        // Assertions
-        cy.get("[data-cy=day]").contains("Wednesday").should("be.visible");
-        cy.get("[data-cy=day]").contains("Thursday").should("not.exist");
-        cy.get("[data-cy=day]").contains("Friday").should("not.exist");   
-    });
-
+    
     it("should filter sessions and only display thursday sessions when Thursday button is clicked", () => {
         cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionsInfo");
         cy.get("@ThursdayBtn").click();
@@ -45,11 +34,12 @@ describe("Sessions page", () => {
     });
 
     it("should filter sessions and only display friday sessions when Friday button is clicked", () => {
-        cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionsInfo");
+        cy.intercept("POST", "http://localhost:4000/graphql", {fixture: "sessions.json"}).as("getSessionsInfo");
         cy.get("@FridayBtn").click();
         cy.wait("@getSessionsInfo")
 
         // Assertions
+        cy.get("[data-cy=day]").should("have.length", 4);
         cy.get("[data-cy=day]").contains("Wednesday").should("not.exist");
         cy.get("[data-cy=day]").contains("Thursday").should("not.exist");
         cy.get("[data-cy=day]").contains("Friday").should("be.visible");   
